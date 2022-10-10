1e7d08df861cb87cff8feab257631707f7834636
Khushboo Jain <kjain3437@gmail.com>
2022-10-10 13:11:36 +0530

    created application, customer, Customer service, dAo Service, MenuItem, order, order service vendor, vendor service

---
+++ src/main/java/com/bootcoding/java/app/Application.java
@@ -0,0 +1,48 @@
+package com.bootcoding.java.app;
+
+import com.bootcoding.java.dao.DAOService;
+import com.bootcoding.java.model.Customer;
+import com.bootcoding.java.service.CustomerService;
+import com.bootcoding.java.service.OrderService;
+import com.bootcoding.java.service.VendorService;
+
+public class Application {
+
+    public static void main(String[] args) {
+
+        DAOService dbService = new DAOService();
+        dbService.printCustomerData();
+        System.out.println("********************************************************");
+        dbService.printVendorData();
+//
+//        Customer Khushboo = new Customer();  //customer object
+//        Khushboo.setName("Khushboo Jain");
+//        Khushboo.setCity("Nagpur");
+//        Khushboo.setDeliveryAddress("Nagpur, Maharashtra");
+//        Khushboo.setEmailId("kjain3437@gmail.com");
+//        Khushboo.print();
+//        Customer Vishakha = new Customer();
+//        Vishakha.setName("Vishakha Jain");
+//        Vishakha.setCity("Akola");
+//        Vishakha.setDeliveryAddress("Akola, Maharashtra");
+//        Vishakha.setEmailId("vishakhaj320@gmail.com");
+//        Vishakha.print();
+//        Customer Himesh = new Customer();
+//        Himesh.setName("Himesh Jain");
+//        Himesh.setCity("Aspur");
+//        Himesh.setDeliveryAddress("Aspur, Maharashtra");
+//        Himesh.setEmailId("himeshj320@gmail.com");
+//        Himesh.print();
+//
+//        OrderService orderService = new OrderService();
+//        orderService.createOrder();   //createOrder method called
+//
+//        CustomerService  customerService = new CustomerService();
+//        customerService.registerCustomer();
+//
+//        VendorService vendorService =new VendorService();
+//        vendorService.registerVendor();
+
+
+    }
+}
